38fdb2a8b7f36e0545925305be62014f8f29562d
josuellions <josuellions@gmail.com>
2024-12-21 14:52:01 -0300

    add view e head `summary`

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -1,3 +1,24 @@
+import { Plus } from 'lucide-react'
+
+import { DialogTrigger } from '@radix-ui/react-dialog'
+import { Button } from './ui/button'
+import { InOrbitIcon } from './in-orbit-icon'
+
 export function Summary() {
-  return <h2>Summary</h2>
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <InOrbitIcon />
+          <h2>5 a 10 de Agosto</h2>
+        </div>
+        <DialogTrigger asChild>
+          <Button>
+            <Plus className="size-5" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+    </div>
+  )
 }
