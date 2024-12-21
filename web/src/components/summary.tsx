4e627c3fcd3c0b7cad270ed5d6a398100810ed48
josuellions <josuellions@gmail.com>
2024-12-21 15:16:30 -0300

    add lista de metas concluidas `summary`

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -1,4 +1,4 @@
-import { Plus } from 'lucide-react'
+import { CheckCircle2, Plus } from 'lucide-react'
 
 import { DialogTrigger } from '@radix-ui/react-dialog'
 import { Button } from './ui/button'
@@ -50,12 +50,32 @@ export function Summary() {
         <Separator />
 
         <div className="flex gap-3 flex-wrap">
-          {goals.map((goal, index) => (
-            <OutlineButton key={`${index}`}>
+          {goals.map(goal => (
+            <OutlineButton key={goal.title}>
               <Plus className="size-4 text-zinc-600" />
               {goal.title}
             </OutlineButton>
           ))}
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <h2>Sua semana</h2>
+          <div className="flex flex-col gap-4">
+            <h3 className="font-medium">
+              Domingo{' '}
+              <span className="text-zinc-400 text-xs">(10 de agosto)</span>
+            </h3>
+            <ul className="flex flex-col gap-3">
+              <li className="flex items-center gap-2">
+                <CheckCircle2 className="size-4 text-pink-500" />
+                <span className="text-sm text-zinc-400">
+                  Você completou "
+                  <span className="text-zinc-100">Acordar cedo</span>" às{' '}
+                  <span className="text-zinc-100">08:13h</span>
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
