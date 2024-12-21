652c420209122cf7cddcd98b5fed17ba8424779a
josuellions <josuellions@gmail.com>
2024-12-21 14:58:36 -0300

    add progress-bar metas `summary`

--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -3,6 +3,8 @@ import { Plus } from 'lucide-react'
 import { DialogTrigger } from '@radix-ui/react-dialog'
 import { Button } from './ui/button'
 import { InOrbitIcon } from './in-orbit-icon'
+import { Progress, ProgressIndicator } from './ui/progress-bar'
+import { Separator } from './ui/separator'
 
 export function Summary() {
   return (
@@ -18,6 +20,22 @@ export function Summary() {
             Cadastrar meta
           </Button>
         </DialogTrigger>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Progress max={15} value={8}>
+          <ProgressIndicator style={{ width: '8px' }} />
+        </Progress>
+
+        <div className="flex items-center justify-between text-xs text-zinc-400">
+          <span>
+            Você completou <span className="text-zinc-100">8</span> de{' '}
+            <span className="text-zinc-100">15</span> metas nessa semana.
+          </span>
+          <span>58%</span>
+        </div>
+
+        <Separator />
       </div>
     </div>
   )
